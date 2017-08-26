92dc5c18aac2b12a413e0eabca3bc74e91576a01
Adam Holtzman <adam.holtzman@gmail.com>
2017-08-26 13:40:47 -0400

    work on music player page

--- src/Paths/Music.js
+++ src/Paths/Music.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import Album1 from '../components/Album1'
+import Album2 from '../components/Album2'
+import BG from '../components/BG'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 30px;
+  padding-bottom: 60px;
+`
 
 const Music = () => {
   return(
-    <div>
-      music page
-    </div>
+    <BG>
+    <Wrapper>
+      <Album2 />
+      <Album1 />
+    </Wrapper>
+  </BG>
   )
 }
 
---
+++ src/components/AlbumWrap.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid dimgrey;
+  height: 100%;
+  width: 500px;
+  background-color: #333;
+  padding-top: 30px;
+
+  h3 {
+    color: white;
+  }
+`
+
+export default Wrapper
