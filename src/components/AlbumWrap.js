d32d0dd6fb45eb383fefdf647a865f10321d240f
Adam Holtzman <adam.holtzman@gmail.com>
2017-08-26 14:56:32 -0400

    style change

--- src/components/AlbumWrap.js
+++ src/components/AlbumWrap.js
@@ -4,10 +4,10 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border: 1px solid dimgrey;
+  border: 1px solid grey;
   height: 100%;
   width: 500px;
-  background-color: #333;
+  background-color: #222;
   padding-top: 30px;
 
   h3 {
